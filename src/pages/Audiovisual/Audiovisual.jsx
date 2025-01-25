d1b714feaf53c51238d48dc883f46becd04cfc0b
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-01-25 20:00:51 +0100

    FEAT WebDesing General Structure

--- src/pages/Audiovisual/Audiovisual.jsx
+++ src/pages/Audiovisual/Audiovisual.jsx
@@ -1,6 +1,24 @@
 import Header from '../../components/header/header'
 import './Audiovisual.css'
 
+const audiovisualProjects = [
+  {
+    title: 'Papaoutai',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737828796/Porfolio/El-perol-web-design-project.png',
+    path: '/web-design'
+  },
+
+  {
+    title: 'Roots',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830097/Porfolio/Pokemon-cards-webDesign-project.png',
+    path: '/web-design'
+  },
+  {
+    title: 'Jornadas de España en RD',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830329/Porfolio/Retro-games-webDesign-project.png',
+    path: '/web-design'
+  }
+]
 const Audiovisual = () => {
   return (
     <section id='section-audiovisual'>
--- src/pages/webDesign/webDesign.jsx
+++ src/pages/webDesign/webDesign.jsx
@@ -1,11 +1,43 @@
 import Header from '../../components/header/header'
 import './WebDesign.css'
 
+const webDesignProjects = [
+  {
+    title: 'El Perol ',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737828796/Porfolio/El-perol-web-design-project.png',
+    path: '/web-design'
+  },
+
+  {
+    title: 'Pokemon`s Cards ',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830097/Porfolio/Pokemon-cards-webDesign-project.png',
+    path: '/web-design'
+  },
+  {
+    title: 'Retro Games',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830329/Porfolio/Retro-games-webDesign-project.png',
+    path: '/web-design'
+  }
+]
+
 const WebDesign = () => {
   return (
     <section id='section-webDesign'>
       <Header />
       <h2>Web Design</h2>
+      <div className='div-line-title-webDesign'></div>
+      <article className='article-projects-webDesign flex-container'>
+        {webDesignProjects.map((project, index) => (
+          <div
+            key={index}
+            className='div-project-webDesign'
+            onClick={() => handleProject(project.path)}
+          >
+            <img src={project.img} alt={project.title}></img>
+            <h3>{project.title}</h3>
+          </div>
+        ))}
+      </article>
     </section>
   )
 }
