a3292776ba2876e7c949ece973328955588c0fc2
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-01-25 20:28:13 +0100

    FEAT Audiovisual Projects

--- src/pages/Audiovisual/Audiovisual.jsx
+++ src/pages/Audiovisual/Audiovisual.jsx
@@ -4,18 +4,21 @@ import './Audiovisual.css'
 const audiovisualProjects = [
   {
     title: 'Papaoutai',
-    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737828796/Porfolio/El-perol-web-design-project.png',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737831583/Porfolio/Papaoutai-audiovisual-project.png',
+    colaboration: 'Freelance',
     path: '/web-design'
   },
 
   {
     title: 'Roots',
-    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830097/Porfolio/Pokemon-cards-webDesign-project.png',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737832165/Porfolio/Roots-audiovisual-project.png',
+    colaboration: 'Freelance',
     path: '/web-design'
   },
   {
     title: 'Jornadas de España en RD',
-    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830329/Porfolio/Retro-games-webDesign-project.png',
+    img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737832582/Porfolio/Semanas-espa%C3%B1a-audiovisual-project.png',
+    colaboration: 'Embajada de España en RD ',
     path: '/web-design'
   }
 ]
@@ -23,7 +26,22 @@ const Audiovisual = () => {
   return (
     <section id='section-audiovisual'>
       <Header />
+      <div className='div-line-title-audiovisual'></div>
       <h2>Audiovisual</h2>
+      <div className='div-line-title-audiovisual'></div>
+      <article className='article-projects flex-container'>
+        {audiovisualProjects.map((project, index) => (
+          <div
+            key={index}
+            className='div-project'
+            onClick={() => handleProject(project.path)}
+          >
+            <img src={project.img} alt={project.title}></img>
+            <h3>{project.title}</h3>
+            <h4>{project.colaboration}</h4>
+          </div>
+        ))}
+      </article>
     </section>
   )
 }
--- src/pages/webDesign/webDesign.jsx
+++ src/pages/webDesign/webDesign.jsx
@@ -5,17 +5,20 @@ const webDesignProjects = [
   {
     title: 'El Perol ',
     img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737828796/Porfolio/El-perol-web-design-project.png',
+    colaboration: 'Bootcamp Rock the Code',
     path: '/web-design'
   },
 
   {
     title: 'Pokemon`s Cards ',
     img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830097/Porfolio/Pokemon-cards-webDesign-project.png',
+    colaboration: 'Bootcamp Rock the Code',
     path: '/web-design'
   },
   {
     title: 'Retro Games',
     img: 'https://res.cloudinary.com/dmztjnlrp/image/upload/v1737830329/Porfolio/Retro-games-webDesign-project.png',
+    colaboration: 'Bootcamp Rock the Code',
     path: '/web-design'
   }
 ]
@@ -24,17 +27,18 @@ const WebDesign = () => {
   return (
     <section id='section-webDesign'>
       <Header />
-      <h2>Web Design</h2>
       <div className='div-line-title-webDesign'></div>
-      <article className='article-projects-webDesign flex-container'>
+      <h2>Web Design</h2>
+      <article className='article-projects flex-container'>
         {webDesignProjects.map((project, index) => (
           <div
             key={index}
-            className='div-project-webDesign'
+            className='div-project'
             onClick={() => handleProject(project.path)}
           >
             <img src={project.img} alt={project.title}></img>
             <h3>{project.title}</h3>
+            <h4>{project.colaboration}</h4>
           </div>
         ))}
       </article>
