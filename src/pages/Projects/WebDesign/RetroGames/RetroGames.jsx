1b3a0ad1e310b4c96abc117cc7a54663bd9f66de
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-03-05 11:39:01 +0100

    Feat Retro Games sections - Article Challenges

--- src/pages/Projects/WebDesign/RetroGames/RetroGames.jsx
+++ src/pages/Projects/WebDesign/RetroGames/RetroGames.jsx
@@ -1,20 +1,25 @@
 import Header from '../../../../components/header/header'
 import './RetroGames.css'
-import { FaSquare } from 'react-icons/fa6'
+import { FaSquare, FaGamepad, FaGears } from 'react-icons/fa6'
+import { FaPlus, FaLongArrowAltRight } from 'react-icons/fa'
+import { MdFluorescent } from 'react-icons/md'
+import { TbMoodCrazyHappyFilled } from 'react-icons/tb'
 
 const retroGamesItems = {
   name: 'Retro Games',
   imgMain:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739905043/Porfolio/Pokemon%20Cards/Pokemon-cards-main-image.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741167833/Porfolio/Retro%20Games/Retro-games-main-image.png',
   type: 'React project - games',
   tools: 'React - Css',
   colaboration: 'Individual - Bootcamp Rock the Code',
   focus:
-    'The main objective of this project was to develop a website that interacted with the Pokémon API through requests. The challenge was to retrieve various data and build a cohesive application while utilizing several React hooks. It was crucial to avoid unnecessary re-renders and to organize the code in a modular way, making the project scalable.',
+    'The goal of this project was to create a website with two games using React. It was essential to avoid unnecessary re-renders while developing clear and scalable code. The website needed to support internal navigation across multiple routes, and the styling was open to creativity.',
   imgFocus1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741079918/Porfolio/Pokemon%20Cards/Pokemon-cards-image-1.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741170334/Porfolio/Retro%20Games/Retro-games-image-1.png',
+  imgFocus2:
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741170334/Porfolio/Retro%20Games/Retro-games-image-2.png',
   challenges:
-    'One of the biggest challenges was how to represent such an iconic image as Pokémon in a simple yet original way, avoiding an obvious or repetitive project. My goal was to revisit the classic Pokémon aesthetic without directly replicating what had already been done. To achieve this, I decided not to make a literal copy, but to draw inspiration from the visual style of the Game Boy consoles, using a more modern color palette and more defined details, creating a reinterpretation of that retro aesthetic.',
+    'One of the major challenges was avoiding unnecessary re-renders. Each game consisted of different components, some of which needed to be updated as the game progressed to ensure a constant refresh. However, others did not need to be re-rendered since their content remained unchanged. For this reason, it was essential to properly define the components and their respective functions.',
   imgChallenge1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741081206/Porfolio/Pokemon%20Cards/Pokemon-cards-image-2.png',
   imgChallenge2:
@@ -22,7 +27,7 @@ const retroGamesItems = {
   imgChallenge3:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741081733/Porfolio/Pokemon%20Cards/Pokemon-cards-image-4.png',
   process1:
-    'The first step was to research the different graphical representations of Pokémon over the years in various video games across different consoles. I wanted to understand how the graphics had been adapted to different technologies and screens throughout the eras. This research allowed me to better grasp the retro style and decide how to adapt it into a more current visual approach, maintaining the essence without resorting to cheap nostalgia.',
+    'Los juegos elegidos fueron el Bingo y el Tres en Raya. El primer proceso fue definir cómo iban a desarrollar ambos juegos teniendo en cuenta la funcionalidad que debían tener, en especial el Tres en Raya ya que juegas contra otro jugador. Finalmente para ambos me decanté por exponer un conjunto de variables posible y que el juego las utilizara de manera aleatoria. De esa manera da la sensación de jugar contra un contrincante aunque no sea del todo así.',
   imgProcess1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741084357/Porfolio/Pokemon%20Cards/Pokemon-cards-image-5.png',
   imgProcess2:
@@ -30,12 +35,12 @@ const retroGamesItems = {
   imgProcess3:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741084351/Porfolio/Pokemon%20Cards/Pokemon-cards-image-7.png',
   process2:
-    'Next, I dived into studying the API. I read its documentation thoroughly, learned how to make requests, and explored the available data. Since the API offered an enormous amount of information, I had to carefully organize and select the Pokémon data I wanted to display, as well as narrow down which Pokémon to show, given the extensive variety.',
+    'El siguiente paso fue definir los componentes y dividir las funcionalidades de cada uno. Ya que cada juego debía dar una respuesta ante la acción del jugador, debía tener componentes en una escucha constante pero evitar que los que no eran necesarios no se re-renderizaran constantemente.',
   imgProcess4:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741083595/Porfolio/Pokemon%20Cards/Pokemon-cards-scheme.png',
   process3:
-    'Once the data selection was clear, I developed the project using React and CSS for styling. I conducted in-depth research on components, re-renders, and React hooks. Although the project was relatively simple, my goal was to create clean, scalable, and well-structured code so that I could gain a solid understanding of React’s fundamentals and apply that knowledge to more complex projects in the future.',
-  extraGame:
+    'Una vez, la parte funcional del proyecto estaba definidida me centré en el diseño. Decidí insipirarme en un diseño neón, haciendo referencia a los recreativos que solían estar inundados de carteles neón y las propias máquinas también tenían ese tipo de luz y colores.',
+  extraRoots:
     'As an extra touch, I wanted to pay special attention to the user experience. Although it was an informational site with no complex interactions, I thought it was important for the user to enjoy some simple interactivity. I implemented a small game on the homepage: when the user clicks on the Pokéball, Pokémon randomly appear across the screen. Additionally, hover effects on the cards change the shadows of the containers, and clicking on each Pokémon allows the user to rotate its image. These small details make the site more enjoyable and spark user curiosity to discover more.',
   gameVideo:
     'https://res.cloudinary.com/dmztjnlrp/video/upload/v1741088371/Porfolio/Pokemon%20Cards/Pokemon-cards-game-video.mov',
@@ -76,6 +81,45 @@ const RetroGames = () => {
           ))}
           <FaSquare className='square-green-last' />
         </ul>
+      </article>
+      <article className='article-retro-games-focus'>
+        <h3>Project Focus</h3>
+        <div className='div-scheme-retro-games'>
+          <FaGamepad className='big-icon-scheme-web-design' />
+          <FaPlus className='small-icon-scheme-web-design' />
+          <FaGears className='big-icon-scheme-web-design' />
+          <FaPlus className='small-icon-scheme-web-design' />
+          <MdFluorescent className='big-icon-scheme-web-design' />
+          <FaLongArrowAltRight className='small-icon-scheme-web-design' />
+          <TbMoodCrazyHappyFilled className='big-icon-scheme-web-design' />
+        </div>
+        <p>{retroGamesItems.focus}</p>
+        <div className='div-images-focus-retro-games flex-container'>
+          <img src={retroGamesItems.imgFocus1} alt='Bingo' />
+          <img src={retroGamesItems.imgFocus2} alt='Tres en raya' />
+        </div>
+      </article>
+      <article className='article-retro-games-challenges flex-container'>
+        <h3>Challenges</h3>
+        <p>{retroGamesItems.challenges}</p>
+        <div className='div-retro-games-challenges-img flex-container'>
+          <div>
+            <img
+              src={retroGamesItems.imgChallenge1}
+              alt='Website color palette'
+            />
+          </div>
+          <div className='flex-container'>
+            <img
+              src={retroGamesItems.imgChallenge2}
+              alt='Main page - Selection hover'
+            />
+            <img
+              src={retroGamesItems.imgChallenge3}
+              alt='About part of the website'
+            />
+          </div>
+        </div>
       </article>
     </section>
   )
