6f76891e26971107bb5712f455905be6b39c278f
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-03-11 20:27:28 +0100

    Feat Papaoutai Section Texts

--- src/pages/Projects/Audiovisual/Papaoutai/Papaoutai.jsx
+++ src/pages/Projects/Audiovisual/Papaoutai/Papaoutai.jsx
@@ -10,41 +10,45 @@ const papaoutaiItems = {
   type: 'Videodance project',
   tools: 'Adobe Illustrator - Davinci Resolve',
   colaboration: 'Freelance - Creand¡Oh! - Fernando Corredoira',
-  focus:
-    'The main objective of this project was to develop an interactive web application focused on traditional Spanish cuisine recipes. User experience was key, so functionalities were integrated to allow dynamic and personalized interaction with the content. In addition, the platform aimed to stand out from other recipe websites by implementing an innovative and appealing design, aimed at capturing and maintaining the interest of the target audience. The combination of usability, aesthetics, and functionality was essential to provide a unique and engaging experience.',
+  focus1:
+    'The main concept is the representation of the emotions experienced by a person in the absence of a father or mother in their life. These emotions range from deep anger to love and admiration. This personal conflict is expressed through the movement of dance, complemented by audiovisual tools.',
+  focus2:
+    'The setting recreates a somewhat aseptic environment, resembling a pavilion, yet with subtle references to playfulness and childhood. The lighting design and styling add a sense of humanity, creating a contrast between the beauty and delicacy of love and the harsh reality of anger and resentment. The choreography, reinforced by camera movements and various framing techniques, takes us through the different emotional stages that the person experiences.',
+  focus3:
+    'The song accompanying this piece is Papaoutai by Stromae, which tells the story of a child demanding answers from his father about why he doesn’t spend time with him. The artist questions the difference between the role of a progenitor and that of a father, as well as the impact of certain men’s and women’s actions on the lives of their children. This videodance piece specifically explores the emotional rollercoaster that the child undergoes, along with the doubts and personal crises they face. References to the original work are made through the final color grading and certain movements within the choreography.',
   imgFocus1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1738695178/Porfolio/El%20Perol/Elperol-image2.png',
   imgFocus2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1738697207/Porfolio/El%20Perol/ElPerol-image3.png',
   challenges:
-    "One of the most significant challenges was managing the database information dynamically while ensuring that the web's visual structure remained consistent. It was crucial to maintain precise control over margins, proportions, and content containers, so that despite allowing users the flexibility to add content, the aesthetics and layout of the interface were not compromised.",
+    'The main challenge of this project was to represent an abstract concept— the feeling of loss towards a father or mother—through a videodance piece. It was essential to ensure that both the audiovisual language and the language of dance conveyed the same concept while also complementing and reinforcing each other.',
   imgChallenge1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1738867617/Porfolio/El%20Perol/ElPerol-image4.png',
-  process1:
-    'First, I conducted research where I explored the most visited recipe websites, identifying what information they displayed, how they presented it, and what the user interaction and experience were like. My conclusion was that these websites are very clear and intuitive, but in terms of design, they are quite similar to basic blogs, offering a user experience that feels underdeveloped. They primarily focus on functionality. This is where I find the opportunity to provide a different approach that would set El Perol apart from other recipe websites. Later, I expanded my research to include projects and websites with more innovative designs focused on gastronomy in general—restaurants, food product websites, food magazines, and others. I wanted to understand how other designers presented food and what their unique proposals were. With these two approaches in mind, I began to build my concept.',
-  imgProcess1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739473271/Porfolio/El%20Perol/El-perol-scheme.png',
-  process2:
-    'Next, I developed the backend: the recipe database, the types of users I wanted to have, how they would interact, and what actions they could perform based on their user type. This way, I created the project’s framework. There would be three types of users: unregistered users, registered users with a basic account, and registered users with a professional account. Each type would have access to specific information and varying levels of customization. I placed significant emphasis on the admin functions and the content validation tasks. The user flows had to be clear and ensure a functional connection between the different database models. ',
-  imgProcess2:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739393373/Porfolio/El%20Perol/ElPerol-image5.png',
-  process3:
-    'With the backend developed and the main design and user experience ideas in place, I began shaping the project visually through the frontend. I used React, as I felt it offered the flexibility I needed for this type of project, along with the styling flexibility CSS provides. Since it was an individual project, this final phase gave me the space to make some adjustments and, most importantly, to incorporate additional ideas.',
-  adminRole:
-    "The role of administrator has been fundamental in the development of the project. Recipe posts must be validated before being published by the administrator to maintain the quality of the content. Restaurant accounts also go through the same verification process to prevent fake accounts and ensure the website's credibility. Additionally, the administrator role has functions that allow them to delete accounts and recipes. Therefore, a section has been created to host all these administrative functions, with the same design as the rest of the website, providing a user-friendly and simple experience.",
+  references1:
+    'First, I selected several references to develop the project. I chose two choreographers who direct their own choreographic videos: Andrew Winghart and Keone Madrid. They address social themes and use audiovisual creation to emphasize the meaning of their dance. Additionally, their approach to camera work, lighting, and art direction is very similar to the overall concept of this project.',
+  references2:
+    "Regarding the camera work, they use fixed lenses and, in some cases, wide-angle lenses to capture the full group of dancers when their number is large. The focal lengths are short, as they integrate the available architectural elements into the composition of the image, avoiding background blurring. Every framing choice and camera movement is designed not only to emphasize the choreography but also to seamlessly connect the background with the dancer’s movement, using the geometry of the location and the body's directional lines.",
+  references3:
+    'The lighting adds a sense of humanity to the piece, often utilizing overhead or backlighting to create dreamlike atmospheres, resembling theatrical proposals. In videos with natural daylight lighting, soft lights are used, along with elements that cast distinct shadows, creating patterns that add texture to the image. As for costume and makeup, pastel tones are predominant, complementing the surroundings. The locations are real places rather than studio sets or excessively artificial environments, making the scenes feel more relatable and reinforcing the themes explored.',
+  location:
+    'The recording took place in the pavilion located in the Espartales Sports Complex in Alcalá de Henares. The lighting was entirely natural, as the space has large openings that allow ample light, aligning with the desired visual aesthetic. The use of artificial lighting was not considered, as the size of the location would have required high-powered lights to significantly alter the lighting quality.',
   imgAdmin1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819188/Porfolio/El%20Perol/El-perol-admin1.png',
   imgAdmin2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819188/Porfolio/El%20Perol/El-perol-admin2.png',
   imgAdmin3:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819189/Porfolio/El%20Perol/El-perol-admin4.png',
+  camera:
+    'Except for some movements and positions defined in the shot list, there are no overly strict camera plans. Instead, the camera zone, movement direction, and main axes will be defined. It is important to maintain a clear reference to the center of the choreographic positions. With these camera movements, wide, medium, and close-up shots will be captured, along with the indications outlined in the shot list.',
+  colorGrading:
+    'The idea was to reference the look of the original music video, creating a visual nod to the piece. A primary color correction was performed by adjusting highlights, shadows, and midtones to achieve the widest possible range. Contrast and saturation were fine-tuned, and the overall color balance was adjusted. In the secondary correction, the most intense green tones were neutralized. Finally, the look was refined using a still from the original music video and adapting it to match the tones and intensities present in our footage.',
   learning:
-    'For me, the biggest lesson from this project has been managing to contribute something new to such a well-defined type of website, and ensuring that these innovations are based on real improvements. At first, it was a big challenge to come up with an idea that would add value and be useful to this type of website, but thanks to the research work, I was able to clearly identify the available improvements. Therefore, I consider the preliminary research work to be essential.',
+    'The limitations in terms of space, budget, and equipment pushed me to put extra effort into the pre-production phase to achieve the level of quality I aimed for. This experience gave me a deeper understanding of the importance of pre-shooting tasks and how essential communication between different departments is.',
   mistakes:
-    "Regarding the mistakes, I believe I should have spent more time defining the workflows within the website. Initially, I had a general idea in place, and as the project progressed, I made some modifications that led to changes in both the backend and frontend simultaneously, resulting in slower progress. It is crucial to have all workflows defined and finalized before starting development, even if it's an individual project.",
+    'Regarding mistakes, I did not take into account certain colors present at the filming location. Since it was a sports complex, there were some highly vibrant elements that made the color grading process more challenging. I had to isolate these elements to reduce their saturation and intensity without affecting the rest of the image. I believe that in a longer project, this issue could have significantly delayed the editing process. This could have been avoided by conducting test shots beforehand to determine whether the location was suitable.',
   imgLearning:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739820184/Porfolio/El%20Perol/El-perol-learning-image.png',
-  link: 'https://el-perol-finisher-react-idps.vercel.app'
+  link: 'https://www.youtube.com/watch?v=918lYUKCCzA'
 }
 const papaoutaiDetails = [
   { icon: <FaSquare className='square-green' />, text: papaoutaiItems.type },
