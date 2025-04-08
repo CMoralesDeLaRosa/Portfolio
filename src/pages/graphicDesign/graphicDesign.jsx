0f8abde2cc65c6133101b7f3c12526ed21adaff4
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-04-08 20:35:39 +0200

    Fix Route CSS Graphic Design

--- src/pages/graphicDesign/graphicDesign.jsx
+++ src/pages/graphicDesign/graphicDesign.jsx
@@ -1,6 +1,6 @@
 import Footer from '../../components/footer/footer'
 import Header from '../../components/header/header'
-import './GraphicDesign.css'
+import './graphicDesign.css'
 
 const graphicDesignProjects = [
   {
