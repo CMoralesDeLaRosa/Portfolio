5e4d1505b364a41a6a41121a216c28c9125db980
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-04-09 20:04:28 +0200

    Feat Mobile Menu Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import './header.css'
 import { NavLink } from 'react-router-dom'
+import { HiMenu } from 'react-icons/hi'
 
 const headerItems = ['Web Design', 'Audiovisual', 'Graphic Design', 'About']
 
 const Header = () => {
+  const [isMobileMenuVisible, setMobileMenuVisible] = useState(false)
+
+  const handleMobileMenuToggle = () => {
+    setMobileMenuVisible(!isMobileMenuVisible)
+  }
   return (
     <header id='header' className='flex-container'>
       <nav>
@@ -20,7 +27,26 @@ const Header = () => {
           ))}
         </ul>
       </nav>
-      <div className='div-line-header'></div>
+      <div
+        className='mobile-menu-icon flex-container'
+        onClick={handleMobileMenuToggle}
+      >
+        <HiMenu />
+      </div>
+      <div className={`mobile-menu ${isMobileMenuVisible ? 'visible' : ''}`}>
+        <ul className='flex-container'>
+          {headerItems.map((item) => (
+            <li key={item} className='li-header flex-container'>
+              <NavLink
+                to={`/${item.toLowerCase().replace(' ', '-')}`}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+              >
+                {item}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   )
 }
