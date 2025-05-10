5ad7b83d055f55ea14dd61d6a33655cbdcb05512
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-05-10 13:25:08 +0200

    Feat Section Semanas Espana - Article Main and Article Focus

--- src/pages/Projects/Audiovisual/JornadasEspanaRD/JornadasEspanaRD.jsx
+++ src/pages/Projects/Audiovisual/JornadasEspanaRD/JornadasEspanaRD.jsx
@@ -3,78 +3,60 @@ import Header from '../../../../components/header/header'
 import './JornadasEspanaRD.css'
 import { FaSquare } from 'react-icons/fa6'
 import { FaPlus, FaLongArrowAltRight } from 'react-icons/fa'
-import { GiCrystalGrowth } from 'react-icons/gi'
-import { GiCrystalize } from 'react-icons/gi'
-import { PiSneakerMoveFill } from 'react-icons/pi'
-import { BsCameraReelsFill } from 'react-icons/bs'
+import { GiTeamUpgrade } from 'react-icons/gi'
+import { FaPlane } from 'react-icons/fa'
+import { FaPeopleCarry } from 'react-icons/fa'
+import { MdSlideshow } from 'react-icons/md'
 import { BsStars } from 'react-icons/bs'
 import { useEffect } from 'react'
 
 const jornadasEspanaRDItems = {
   name: 'Jornadas de España en RD',
   imgMain:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746728692/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-Main-Image.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746874955/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-Main-Image.png',
   type: 'Promotional Videos',
   tools: 'Adobe Illustrator - Davinci Resolve',
   colaboration:
     'Freelance - Embajada de España en la República Dominicana - ¡Oh!PROJECT',
   focus1:
-    'The main concept of this project is the representation of the emotions experienced by a person in the absence of a father or mother in their life. These emotions range from deep anger to love and admiration. This personal conflict is expressed through the movement of dance, complemented by audiovisual tools.',
+    'The objective of this project was to produce a series of promotional videos for the Spain Weeks in the Dominican Republic, with the purpose of publicizing the event schedule. The videos were designed to appeal to a wide audience in order to encourage strong attendance at the various activities.',
   imgFocus1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741721712/Porfolio/Papaoutai/Papaoutai-image1.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746875441/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-image1.png',
   imgFocus2:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741721712/Porfolio/Papaoutai/Papaoutai-image2.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746875632/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-image2.png',
   imgFocus3:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741721712/Porfolio/Papaoutai/Papaoutai-image3.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746875681/Porfolio/Semanas de España en RD/Semanas-España-RD-image3.png',
+  imgFocus4:
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746875989/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-image4.png',
+  imgFocus5:
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746876071/Porfolio/Semanas%20de%20Espa%C3%B1a%20en%20RD/Semanas-Espa%C3%B1a-RD-image5.png',
   focus2:
-    'The setting recreates a somewhat aseptic environment, resembling a pavilion, yet with subtle references to playfulness and childhood. The lighting design and styling add a sense of humanity, creating a contrast between the beauty and delicacy of love and the harsh reality of anger and resentment. The choreography, reinforced by camera movements and various framing techniques, takes us through the different emotional stages that the person experiences.',
-  focus3:
-    'The song accompanying this piece is Papaoutai by Stromae, which tells the story of a child demanding answers from his father about why he doesn’t spend time with him. The artist questions the difference between the role of a progenitor and that of a father, as well as the impact of certain men’s and women’s actions on the lives of their children. This videodance piece specifically explores the emotional rollercoaster that the child undergoes, along with the doubts and personal crises they face. References to the original work are made through the final color grading and certain movements within the choreography.',
+    'In addition to explaining the nature of the events taking place during the celebration, the videos feature brief interviews with representatives from the organizing and collaborating institutions. These participants share first-hand insights into the objectives of the initiative and what the public can expect to experience at the various events.',
   challenges:
-    'The main challenge of this project was to represent an abstract concept, the feeling of loss towards a father or mother, through a videodance piece. It was essential to ensure that both the audiovisual language and the language of dance conveyed the same concept while also complementing and reinforcing each other.',
+    'The main challenge of this project was carrying it out remotely. I handled the video editing from Spain, which made thorough planning and close collaboration between the filming and editing teams essential. Additionally, the delivery timeline was very tight, requiring an agile and highly efficient workflow.',
   imgChallenge1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741723080/Porfolio/Papaoutai/Papaoutai-image4.png',
-  references1:
-    'First, I selected several references to develop the project. I chose two choreographers who direct their own choreographic videos: Andrew Winghart and Keone Madrid. They address social themes and use audiovisual creation to emphasize the meaning of their dance. Additionally, their approach to camera work, lighting, and art direction is very similar to the overall concept of this project.',
-  imgReferences1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742930540/Porfolio/Papaoutai/Papaoutai-image6.png',
-  imgReferences2:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746641119/Porfolio/Papaoutai/Papoutai-Image1-Mobile.png',
-  references2:
-    "Regarding the camera work, they use fixed lenses and, in some cases, wide-angle lenses to capture the full group of dancers when their number is large. The focal lengths are short, as they integrate the available architectural elements into the composition of the image, avoiding background blurring. Every framing choice and camera movement is designed not only to emphasize the choreography but also to seamlessly connect the background with the dancer’s movement, using the geometry of the location and the body's directional lines.",
-  references3:
-    'The lighting adds a sense of humanity to the piece, often utilizing overhead or backlighting to create dreamlike atmospheres, resembling theatrical proposals. In videos with natural daylight lighting, soft lights are used, along with elements that cast distinct shadows, creating patterns that add texture to the image. As for costume and makeup, pastel tones are predominant, complementing the surroundings. The locations are real places rather than studio sets or excessively artificial environments, making the scenes feel more relatable and reinforcing the themes explored.',
-  camera:
-    'Except for some movements and positions defined in the shot list, there are no overly strict camera plans. Instead, the camera zone, movement direction, and main axes will be defined. It is important to maintain a clear reference to the center of the choreographic positions. With these camera movements, wide, medium, and close-up shots will be captured, along with the indications outlined in the shot list.',
-  imgCamera1:
+  events:
+    'The video series includes a short promotional teaser, a general overview video of the entire celebration, a dedicated piece on the Hispanidad Regatta, and another on the Dalí Exhibition. All the videos share a consistent visual aesthetic and camera movement style to ensure unity across the series. A musical line by the same artist was also selected to provide continuity throughout the pieces.',
+  imgEvents1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1743615449/Porfolio/Papaoutai/Papaoutai-camera-scheme.png',
-  imgCamera2:
+  imgEvents2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742846332/Porfolio/Papaoutai/Papaoutai-image7.png',
-  colorGrading:
-    'The idea was to reference the look of the original music video, creating a visual nod to the piece. A primary color correction was performed by adjusting highlights, shadows, and midtones to achieve the widest possible range. Contrast and saturation were fine-tuned, and the overall color balance was adjusted. In the secondary correction, the most intense green tones were neutralized. Finally, the look was refined using a still from the original music video and adapting it to match the tones and intensities present in our footage.',
-  imgColorGrading1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742930048/Porfolio/Papaoutai/Papaoutai-image8.png',
-  imgColorGrading2:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742930119/Porfolio/Papaoutai/Papaoutai-image9.png',
-  imgColorGrading3:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742931164/Porfolio/Papaoutai/Papoutai-color-grading.png',
-  imgColorGrading4:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746641559/Porfolio/Papaoutai/Papaoutai-Color-Grading-Mobile.png',
-
+  events2:
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1743615449/Porfolio/Papaoutai/Papaoutai-camera-scheme.png',
   learning:
-    'The limitations in terms of space, budget, and equipment pushed me to put extra effort into the pre-production phase to achieve the level of quality I aimed for. This experience gave me a deeper understanding of the importance of pre-shooting tasks and how essential communication between different departments is.',
+    'Due to the tight delivery timeline, the pre-production phase was minimal, requiring daily coordination, planning, filming, and editing. This made the project significantly more complex than it might appear at first glance, given the short duration of the videos. However, this situation allowed me to develop the ability to work efficiently under pressure and to solve unforeseen issues quickly. It also taught me the importance of adapting when original plans cannot be executed, by promptly finding high-quality alternatives.',
   imgLearning1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742931682/Porfolio/Papaoutai/Papoutai-image10.png',
   imgLearning2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742931682/Porfolio/Papaoutai/Papaoutai-image11.png',
   mistakes:
-    'Regarding mistakes, I did not take into account certain colors present at the filming location. Since it was a sports complex, there were some highly vibrant elements that made the color grading process more challenging. I had to isolate these elements to reduce their saturation and intensity without affecting the rest of the image. I believe that in a longer project, this issue could have significantly delayed the editing process. This could have been avoided by conducting test shots beforehand to determine whether the location was suitable.',
-
+    'Regarding areas for improvement, the limited delivery timeframe affected certain aspects of video quality—particularly the audio. While efforts were made to achieve the best possible sound for both interviews and voice-overs, the time constraints prevented further refinement. For future projects, I have learned that there are methods to obtain higher audio quality while also reducing post-production time.',
   imgMistakes:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1742932468/Porfolio/Papaoutai/Papaoutai-image12.png',
   link: 'https://www.youtube.com/watch?v=918lYUKCCzA'
 }
-const papaoutaiDetails = [
+const jornadasEspanaRDDetails = [
   {
     icon: <FaSquare className='square-green' />,
     text: jornadasEspanaRDItems.type
@@ -106,7 +88,7 @@ const JornadasEspanaRD = () => {
         </div>
         <h2>{jornadasEspanaRDItems.name}</h2>
         <ul className='ul-semanas-espana-main flex-container'>
-          {papaoutaiDetails.map((item, index) => (
+          {jornadasEspanaRDDetails.map((item, index) => (
             <li key={index} className='flex-container'>
               {item.icon}
               {item.text}
@@ -115,34 +97,37 @@ const JornadasEspanaRD = () => {
           <FaSquare className='square-green-last' />
         </ul>
       </article>
-      <article className='article-papaoutai-focus'>
+      <article className='article-semanas-espana-focus'>
         <h3>Project Focus</h3>
-        <div className='div-scheme-papaoutai'>
-          <GiCrystalize className='big-icon-scheme-audiovisual' />
+        <div className='div-scheme-semanas-espana'>
+          <FaPlane className='big-icon-scheme-audiovisual' />
           <FaPlus className='small-icon-scheme-audiovisual' />
-          <PiSneakerMoveFill className='big-icon-scheme-audiovisual' />
+          <MdSlideshow className='big-icon-scheme-audiovisual' />
           <FaPlus className='small-icon-scheme-audiovisual' />
-          <BsCameraReelsFill className='big-icon-scheme-audiovisual-camera' />
+          <FaPeopleCarry className='big-icon-scheme-audiovisual-camera' />
           <FaLongArrowAltRight className='small-icon-scheme-audiovisual' />
-          <GiCrystalGrowth className='big-icon-scheme-audiovisual' />
+          <GiTeamUpgrade className='big-icon-scheme-audiovisual' />
         </div>
         <p>{jornadasEspanaRDItems.focus1}</p>
-        <div className='div-images-focus-papaoutai flex-container'>
+        <div className='div-images-focus-semanas-espana1 flex-container'>
           <img
             src={jornadasEspanaRDItems.imgFocus1}
-            alt='Frame of Papaoutai video'
+            alt='Frame of promotional video'
           />
           <img
             src={jornadasEspanaRDItems.imgFocus2}
-            alt='Frame of Papaoutai video'
+            alt='Frame of Regata de la Hispanidad video'
           />
           <img
             src={jornadasEspanaRDItems.imgFocus3}
-            alt='Frame of Papaoutai video'
+            alt='Frame of Salvador Dalí, ilustrando genios video'
           />
         </div>
         <p>{jornadasEspanaRDItems.focus2}</p>
-        <p>{jornadasEspanaRDItems.focus3}</p>
+        <div className='div-images-focus-semanas-espana2 flex-container'>
+          <img src={jornadasEspanaRDItems.imgFocus4} alt='Frame of interview' />
+          <img src={jornadasEspanaRDItems.imgFocus5} alt='Frame of interview' />
+        </div>
       </article>
       <article className='article-papaoutai-challenges flex-container'>
         <h3>Challenges</h3>
