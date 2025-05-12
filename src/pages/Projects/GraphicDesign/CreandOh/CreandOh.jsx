d3a9623992fddcbb33d8a23737018e502ca241bd
Cristina Morales <cristina.moralesdelarosa@gmail.com>
2025-05-12 21:12:26 +0200

    Feat Section CreandOh

--- src/pages/Projects/GraphicDesign/CreandOh/CreandOh.jsx
+++ src/pages/Projects/GraphicDesign/CreandOh/CreandOh.jsx
@@ -3,9 +3,6 @@ import './CreandOh.css'
 import { FaSquare } from 'react-icons/fa6'
 import { IoBulb } from 'react-icons/io5'
 import { FaPlus, FaLongArrowAltRight } from 'react-icons/fa'
-import { TbExchange } from 'react-icons/tb'
-import { BsPersonArmsUp } from 'react-icons/bs'
-import { AiFillThunderbolt } from 'react-icons/ai'
 import { AiOutlineGift } from 'react-icons/ai'
 import Footer from '../../../../components/footer/footer'
 import { useEffect } from 'react'
@@ -23,39 +20,31 @@ const creandOhItems = {
     'Adobe Illustrator - Adobe Photoshop - Adobe Premiere Pro - Davinci Resolve',
   colaboration: 'Freelance',
   focus:
-    'This project focused on developing the visual identity for Cread¡Oh!, a performing arts and dance school born from the merger of two associations: Mopka and ¡Oh!PROJECT!. One of the main goals was to preserve the essence of both original brands, blending them into a new, cohesive, and recognizable identity. Additionally, it was essential to create a logo that represented the wide range of artistic disciplines offered by the school and resonated with its diverse target audiences.',
+    'This project focused on developing the visual identity for Cread¡Oh!, a performing arts and dance school born from the merger of two associations: Mopka and ¡Oh!PROJECT!. One of the main goals was to preserve the essence of both original brands, blending them into a new, cohesive, and recognizable identity. Additionally, it was essential to create a logo and a visual identity that represented the wide range of artistic disciplines offered by the school and resonated with its diverse target audiences.',
   imgFocus1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1747073722/Porfolio/Creand%C2%A1Oh%21/Creando-image1.png',
   imgFocus2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1747074499/Porfolio/Creand%C2%A1Oh%21/CreandOh-image2.jpg',
   imgFocus3:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1747074997/Porfolio/Creand%C2%A1Oh%21/CreandOh-image3.jpg',
-  challenges:
-    "One of the most significant challenges was managing the database information dynamically while ensuring that the web's visual structure remained consistent. It was crucial to maintain precise control over margins, proportions, and content containers, so that despite allowing users the flexibility to add content, the aesthetics and layout of the interface were not compromised.",
+  challenges1:
+    "One of the main challenges was merging both logos while preserving the essence of each and conveying that the space represented both projects. Another key difficulty was creating a visual identity that could encompass a wide range of activities, from children's workshops to Hip Hop battle events.",
+  challenges2:
+    'In addition to the logo design, it was essential to maintain a coherent visual identity across all elements related to the school. This included not only the interior design of the space but also the advertising materials and signage. The goal was to create a unified visual identity that connected all aspects of the project, while being flexible enough to adapt to different activities and styles without losing its essence.',
   imgChallenge1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741457522/Porfolio/El%20Perol/ElPerol-image4.png',
+    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1747076451/Porfolio/Creand%C2%A1Oh%21/Creando-image4.png',
   imgChallenge2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746554260/Porfolio/El%20Perol/El-Perol-Scheme-Mobile.png',
   process1:
-    'First, I conducted research where I explored the most visited recipe websites, identifying what information they displayed, how they presented it, and what the user interaction and experience were like. My conclusion was that these websites are very clear and intuitive, but in terms of design, they are quite similar to basic blogs, offering a user experience that feels underdeveloped. They primarily focus on functionality. This is where I find the opportunity to provide a different approach that would set El Perol apart from other recipe websites. Later, I expanded my research to include projects and websites with more innovative designs focused on gastronomy in general—restaurants, food product websites, food magazines, and others. I wanted to understand how other designers presented food and what their unique proposals were. With these two approaches in mind, I began to build my concept.',
+    'To achieve a unified design, I first selected the most representative elements from each visual identity: the isotypes. From there, I chose a standard yet fluid typeface that reflected the artistic nature of the project. Finally, I retained the original colors —purple and yellow— in subtle accents, providing visual continuity while allowing greater flexibility for the new logo.',
   imgProcess1:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739473271/Porfolio/El%20Perol/El-perol-scheme.png',
   process2:
     'Next, I developed the backend: the recipe database, the types of users I wanted to have, how they would interact, and what actions they could perform based on their user type. This way, I created the project’s framework. There would be three types of users: unregistered users, registered users with a basic account, and registered users with a professional account. Each type would have access to specific information and varying levels of customization. I placed significant emphasis on the admin functions and the content validation tasks. The user flows had to be clear and ensure a functional connection between the different database models. ',
   imgProcess2:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1741456600/Porfolio/El%20Perol/ElPerol-image5.png',
-  process3:
-    'With the backend developed and the main design and user experience ideas in place, I began shaping the project visually through the frontend. I used React, as I felt it offered the flexibility I needed for this type of project, along with the styling flexibility CSS provides. Since it was an individual project, this final phase gave me the space to make some adjustments and, most importantly, to incorporate additional ideas.',
   imgProcess3:
     'https://res.cloudinary.com/dmztjnlrp/image/upload/v1746554839/Porfolio/El%20Perol/El-Perol-Scheme-2-Mobile.png',
-  adminRole:
-    "The role of administrator has been fundamental in the development of the project. Recipe posts must be validated before being published by the administrator to maintain the quality of the content. Restaurant accounts also go through the same verification process to prevent fake accounts and ensure the website's credibility. Additionally, the administrator role has functions that allow them to delete accounts and recipes. Therefore, a section has been created to host all these administrative functions, with the same design as the rest of the website, providing a user-friendly and simple experience.",
-  imgAdmin1:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819188/Porfolio/El%20Perol/El-perol-admin1.png',
-  imgAdmin2:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819188/Porfolio/El%20Perol/El-perol-admin2.png',
-  imgAdmin3:
-    'https://res.cloudinary.com/dmztjnlrp/image/upload/v1739819189/Porfolio/El%20Perol/El-perol-admin4.png',
   learning:
     'For me, the biggest lesson from this project has been managing to contribute something new to such a well-defined type of website, and ensuring that these innovations are based on real improvements. At first, it was a big challenge to come up with an idea that would add value and be useful to this type of website, but thanks to the research work, I was able to clearly identify the available improvements. Therefore, I consider the preliminary research work to be essential.',
   mistakes:
@@ -126,13 +115,10 @@ const CreandOh = () => {
       </article>
       <article className='article-creandoh-challenges flex-container'>
         <h3>Challenges</h3>
-        <p>{creandOhItems.challenges}</p>
-        <div className='div-creandoh-challenges-img'>
+        <p>{creandOhItems.challenges1}</p>
+        <div className='div-creandoh-challenges-img1'>
           <img src={creandOhItems.imgChallenge1} />
-          <img
-            className='img-creandoh-challenges-mobile'
-            src={creandOhItems.imgChallenge2}
-          />
+          <p>{creandOhItems.challenges2}</p>
         </div>
       </article>
       <article className='article-creandoh-process flex-container'>
